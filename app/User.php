e1ea8e26004022ee76ac894e855009bbdb62e327
Mohamed Samir <gm.mohamedsamir@gmail.com>
2020-03-26 15:20:44 +0200

    edit

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'phone', 'level', 'group', 'payment_verified'
+        'name', 'email', 'password', 'phone', 'level', 'group', 'payment_verified', 'device_id'
     ];
 
     /**
